652ec6872782dfd148de184867a3d6c26fcb87b6
Nina Frings <nina.frings@unibas.ch>
2025-06-18 11:35:30 +0300

    Intervention active sampling

---
+++ settings.py
@@ -0,0 +1,44 @@
+from os import environ
+
+SESSION_CONFIGS = [
+     dict(
+         name='intervention',
+         app_sequence=['Intervention_de'],
+         num_demo_participants=100,
+     ),
+     dict(
+         name='intervention_en',
+         app_sequence=['Intervention'],
+         num_demo_participants=1000,
+     ),
+
+ 
+]
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
+)
+
+PARTICIPANT_FIELDS = [ 'group_assignment', 'diet', 'electricity', 'recycling', 'food', 'commute', 'vacation',
+                      'task_rounds']
+
+# ISO-639 code
+# for example: de, fr, ja, ko, zh-hans
+LANGUAGE_CODE = 'en'
+
+# e.g. EUR, GBP, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'USD'
+USE_POINTS = False
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+DEMO_PAGE_INTRO_HTML = """ """
+
+SECRET_KEY = '5553960384234'
